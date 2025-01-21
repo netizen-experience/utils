f3a2bf4d155473178edc29249a5ac865947ff895
Ming Yang <mingyang@netizenexperience.com>
2025-01-21 16:05:01 +0800

    refactor(types): Remove errors from types

--- packages/types/src/lib/utils.ts
+++ packages/types/src/lib/utils.ts
@@ -7,33 +7,6 @@ export type RequiredBy<T, K extends keyof T> = Partial<Omit<T, K>> & Required<Pi
 export type NonEmptyArray<T> = [T, ...T[]];
 
 /**
- * Represents a type that can be converted to a string, including string, number, bigint, boolean, null, and undefined.
- */
-export type Stringable = string | number | bigint | boolean | null | undefined;
-
-/**
- * Represents a type that can be converted to a JSON value.
- */
-export type Jsonable =
-  | string
-  | number
-  | boolean
-  | null
-  | undefined
-  | readonly Jsonable[]
-  | { readonly [key: string]: Jsonable }
-  | { toJSON(): Jsonable };
-
-/**
- * Concatenates elements of an array into a string using a specified separator.
- */
-export type Join<A, Sep extends string = ""> = A extends [infer First, ...infer Rest]
-  ? Rest extends []
-    ? `${First & Stringable}`
-    : `${First & Stringable}${Sep}${Join<Rest, Sep>}`
-  : "";
-
-/**
  * Omits properties from a type while preserving discriminated unions
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
