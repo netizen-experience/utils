5e4885f60877474d2691755bff2b31df77832f30
Ming Yang <mingyang@netizenexperience.com>
2025-01-21 15:00:32 +0800

    refactor(aws): Remove unnecessary packages and update exports

--- packages/aws/src/index.ts
+++ packages/aws/src/index.ts
@@ -1,2 +1,10 @@
-export * from "./lib/database";
-export * from "./lib/storage";
+export { getAwsClients } from "./lib/clients";
+
+// Database
+export type { BatchItemParams } from "./lib/database/batch-write";
+export { DynamoError, DynamoErrorCode } from "./lib/database/errors";
+export { defineTable } from "./lib/database/table";
+export { generatePrefixedKey, parsePrefixedKey } from "./lib/database/utils";
+
+// Storage
+export { defineBucket } from "./lib/storage/bucket";
--- packages/aws/src/lib/clients.ts
+++ packages/aws/src/lib/clients.ts
@@ -1,20 +1,14 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { S3Client } from "@aws-sdk/client-s3";
-import { SESv2Client } from "@aws-sdk/client-sesv2";
-import { SFNClient } from "@aws-sdk/client-sfn";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 let dynamo: DynamoDBDocumentClient;
 let s3: S3Client;
-let ses: SESv2Client;
-let sfn: SFNClient;
 
 export function getAwsClients() {
-  if (dynamo && s3 && ses && sfn) return { dynamo, s3, ses, sfn };
+  if (dynamo && s3) return { dynamo, s3 };
 
   dynamo = DynamoDBDocumentClient.from(new DynamoDBClient({}));
   s3 = new S3Client({});
-  ses = new SESv2Client();
-  sfn = new SFNClient();
-  return { dynamo, s3, ses, sfn };
+  return { dynamo, s3 };
 }
